0b8f58c63b76a498618e9bd11f2a8473e16523c9
Anton Diachenko <antondia@wizardsdev.com>
2021-01-21 16:52:57 +0200

    add new Watchers in saga

--- store/middleware/saga.ts
+++ store/middleware/saga.ts
@@ -1,7 +1,9 @@
 import { all } from 'redux-saga/effects';
 import { getPostsWatcher } from '../sagas/postsSaga';
 import { createPostWatcher } from '../sagas/createPostSaga';
+import { getPostWatcher } from '../sagas/getOnePostSaga';
+import { deletePostWatcher } from '../sagas/deletePostSaga';
 
 export function* rootSaga(): Generator {
-  yield all([getPostsWatcher(), createPostWatcher()]);
+  yield all([getPostsWatcher(), createPostWatcher(), getPostWatcher(), deletePostWatcher()]);
 }
