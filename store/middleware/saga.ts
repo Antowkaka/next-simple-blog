44c4c2f5ae6921b4c8f13341aadd248fdb9a7466
Anton Diachenko <antondia@wizardsdev.com>
2021-01-21 20:32:57 +0200

    add updatePostWatcher

--- store/middleware/saga.ts
+++ store/middleware/saga.ts
@@ -3,7 +3,14 @@ import { getPostsWatcher } from '../sagas/postsSaga';
 import { createPostWatcher } from '../sagas/createPostSaga';
 import { getPostWatcher } from '../sagas/getOnePostSaga';
 import { deletePostWatcher } from '../sagas/deletePostSaga';
+import { updatePostWatcher } from '../sagas/updatePostSaga';
 
 export function* rootSaga(): Generator {
-  yield all([getPostsWatcher(), createPostWatcher(), getPostWatcher(), deletePostWatcher()]);
+  yield all([
+    getPostsWatcher(),
+    createPostWatcher(),
+    getPostWatcher(),
+    deletePostWatcher(),
+    updatePostWatcher(),
+  ]);
 }
