0428cf9e9db0c8235ba38ad2829398d10cca0cfc
Anton Diachenko <antondia@wizardsdev.com>
2021-01-21 16:56:49 +0200

    rename Actions file

--- store/actions/clearPostStatusAction.ts
+++
@@ -1,9 +0,0 @@
-import { CLEAR_POST_STATUS } from '../actionTypes';
-import { ClearStatusActionType } from '../../type';
-
-export const clearPostStatusAction = (): ClearStatusActionType => ({
-  type: CLEAR_POST_STATUS,
-  payload: {
-    status: '',
-  },
-});
